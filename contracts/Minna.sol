5bf6222dcd1409effcedf4d0c8bbe12fd6652a9b
lerajk <lerajk44@gmail.com>
2017-10-17 23:35:51 -0400

    contracts

--- contracts/Minna.sol
+++ contracts/Minna.sol
@@ -3,28 +3,48 @@ import './Ownable.sol';
 
 contract Minna {
     
-    uint contributionPoints; //total points collected by member
+    address public owner;
+     modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-    //a struct to represent teams
-    // eg: 2 teams with 5 members each
-    struct teams {
-        
+
+    //a struct to represent a team
+    struct Meam {
+
         bytes32 teamName;
-        uint contributionTotal; //total points for the team
-        mapping (uint => team) numberOfMembers; //total # of members
+        uint contributionTotal; //total points for a team
+        mapping (uint => member) TeamID; //each member registers with a TeamID
         
     }
     
-    //struc that has information for one team
-    struct team {
-        address member; //external publicn address of a particular member
-        bytes32 registerWithTeam; 
+    //struc that has information for one member
+    struct Member {
+
+        address addressMember; //external publicn address of a particular member
+        uint contributionPoints; //total points collected by member
     }
     
-    team[] arrayTeams; 
+    Member[] public members;
+    
+    //team[] arrayTeams; 
     
     //function to register a member to a team
     // eg: david registers himself to the dev team 
+
+    function register(address _addressMember, uint _contributionPoints) public onlyOwner {
+
+        //putting the information of a member inside a stuct - but why?
+       Member memory newMember;
+       newMember.contributionPoints = _contributionPoints;
+ 
+
+       members.push(newMember);
+       return true;
+
+
+    }
     
     
 } //contract ends
